42340775529924f020f31890cd8627307dfdb222
solar-code-it <pepijnheinen2010@gmail.com>
2026-08-16 21:45:19 +0200

    Add AES-GCM encryption and decryption functions.
    Implement file encryption and decryption using AES-GCM.

---
+++ vault.py
@@ -0,0 +1,50 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
+import hashlib
+
+def make_key(password, salt):
+    return hashlib.scrypt(
+        password.encode(),
+        salt=salt,
+        n=16384,
+        r=8,
+        p=1,
+        dklen=32
+    )
+
+def encrypt_file(filename, password):
+    salt= os.urandom(16)
+    key = make_key(password, salt)
+
+    nonce = os.urandom(12)
+
+    with open (filename, "rb") as file:
+         data = file.read()
+
+    encrypted = AESGCM(key).encrypt(nonce, data, None)
+
+    with open(filename + ".enc", "wb") as file:
+        file.write(salt)
+        file.write(nonce)
+        file.write(encrypted)
+
+    
+def decrypt_file(filename, password):
+    with open(filename, "rb") as file:
+        salt = file.read(16)
+        nonce = file.read(12)
+        encrypted = file.read()
+
+    key = make_key(password, salt)
+
+    data = AESGCM(key).decrypt(nonce, encrypted, None)
+
+    output = filename.removesuffix(".enc")
+
+    with open(output, "wb") as file:
+        file.write(data)
+
+password = "test123"
+
+#encrypt_file("test.txt", password)
+decrypt_file("test.txt.enc", password)
