74990ab44704cb2a2786f2602c3ca5913566d769
solar-code-it <pepijnheinen2010@gmail.com>
2026-08-17 12:38:31 +0200

    Enhance password security and vault management
    Updated the password hashing algorithm to use a stronger configuration and added a vault creation mechanism for salt storage.

--- auth.py
+++ auth.py
@@ -2,6 +2,19 @@ import hashlib
 import os
 import json
 
+# Makes vault key.
+def make_key(password, salt):
+    return hashlib.scrypt(
+        password,
+        salt=salt,
+        n=32768,
+        r=8,
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
+    )
+
+# Registers user to user.json.
 def register():
     username = input("Username: ")
     password = input("Password: ").encode()
@@ -11,9 +24,11 @@ def register():
     hashed = hashlib.scrypt(
         password,
         salt=salt,
-        n=16384,
+        n=32768,
         r=8,
-        p=1
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
     )
 
     user_data = {
@@ -34,9 +49,23 @@ def register():
 
     return password
 
+
+# login system + makes vault incase there is no vault.
 def login():
     username = input("Username: ")
     password = input("Password: ").encode()
+
+    if os.path.exists("test.txt.enc"):
+        with open("test.txt.enc", "rb") as file:
+            salt = file.read(16)
+    else:
+        salt = os.urandom(16)
+
+        # Create vault file and save the salt.
+        with open("test.txt.enc", "wb") as file:
+            file.write(salt)
+
+    key = make_key(password, salt)
 
     try:
         with open("users.json", "r") as file:
@@ -55,14 +84,16 @@ def login():
     new_hash = hashlib.scrypt(
         password,
         salt=saved_salt,
-        n=16384,
+        n=32768,
         r=8,
-        p=1
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
     )
 
     if new_hash == saved_hash:
         print("Login successfull.")
-        return username, password
+        return username, key, salt
 
     else:
         print("Username or password is wrong.")
