1ebadbbba1e1c396d8a2dc956ddd03437c15f1c4
solar-code-it <pepijnheinen2010@gmail.com>
2026-08-17 12:42:33 +0200

    Added a comment where to change file name where encrypted version is stored.

--- vault.py
+++ vault.py
@@ -1,31 +1,101 @@
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import hashlib
 import os
+import json
 
-# Encrypts  text.
-def encrypt_file(filename, key, salt):
-    nonce = os.urandom(12)
+# Makes vault key.
+def make_key(password, salt):
+    return hashlib.scrypt(
+        password,
+        salt=salt,
+        n=32768,
+        r=8,
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
+    )
 
-    with open (filename, "rb") as file:
-         data = file.read()
+# Registers user to user.json.
+def register():
+    username = input("Username: ")
+    password = input("Password: ").encode()
 
-    encrypted = AESGCM(key).encrypt(nonce, data, None)
+    salt = os.urandom(16)
 
-    with open(filename + ".enc", "wb") as file:
-        file.write(salt)
-        file.write(nonce)
-        file.write(encrypted)
+    hashed = hashlib.scrypt(
+        password,
+        salt=salt,
+        n=32768,
+        r=8,
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
+    )
 
-# Decrypts text.
-def decrypt_file(filename, key):
-    with open(filename, "rb") as file:
-        salt = file.read(16)
-        nonce = file.read(12)
-        encrypted = file.read()
+    user_data = {
+    "salt": salt.hex(),
+    "hash": hashed.hex()
+    }
 
-    data = AESGCM(key).decrypt(nonce, encrypted, None)
+    try:
+        with open ("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        users = {}
 
-    output = filename.removesuffix(".enc")
+    users[username] = user_data
 
-    with open(output, "wb") as file:
-        file.write(data)
+    with open  ("users.json", "w") as file:
+        json.dump(users, file, indent=4)
 
+    return password
+
+
+# login system + makes vault incase there is no vault.
+def login():
+    username = input("Username: ")
+    password = input("Password: ").encode()
+
+    if os.path.exists("test.txt.enc"):
+        with open("test.txt.enc", "rb") as file:
+            salt = file.read(16)
+    else:
+        salt = os.urandom(16)
+
+        # Create vault file and save the salt.
+        # Change vault name here.
+        with open("test.txt.enc", "wb") as file:
+            file.write(salt)
+
+    key = make_key(password, salt)
+
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        print("no user file found")
+        return
+
+    if username not in users:
+        print("Username or password is wrong.")
+        return
+
+    saved_salt = bytes.fromhex(users[username]["salt"])
+    saved_hash = bytes.fromhex(users[username]["hash"])
+
+    new_hash = hashlib.scrypt(
+        password,
+        salt=saved_salt,
+        n=32768,
+        r=8,
+        p=1,
+        dklen=32,
+        maxmem=128 * 1024 * 1024
+    )
+
+    if new_hash == saved_hash:
+        print("Login successfull.")
+        return username, key, salt
+
+    else:
+        print("Username or password is wrong.")
+        return None
